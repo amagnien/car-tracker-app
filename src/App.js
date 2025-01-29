dff0d9ad7c492e2de2da515cabcb12be222a1317
amagnien <amagnien@gmail.com>
2025-01-29 17:16:25 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Expenses from './pages/Expenses';
@@ -10,30 +10,29 @@ import Analytics from './pages/Analytics';
 import Settings from './pages/Settings';
 import { AuthProvider } from './context/authContext';
 import './styles/global.css';
-import LoginPage from "./pages/Login";
-import SignUpPage from "./pages/SignUp";
+import LoginPage from './pages/Login';
+import SignUpPage from './pages/SignUp';
 
 const App = () => {
-
-    return (
-        <AuthProvider>
-            <Router>
-                <Navbar />
-                <div className="app-container">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/expenses" element={<Expenses />} />
-                    <Route path="/maintenance" element={<Maintenance />} />
-                    <Route path="/fuel" element={<Fuel />} />
-                    <Route path="/analytics" element={<Analytics />} />
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="/login" element={<LoginPage />} />
-                      <Route path="/signup" element={<SignUpPage />} />
-                  </Routes>
-                </div>
-            </Router>
-      </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/expenses" element={<Expenses />} />
+            <Route path="/maintenance" element={<Maintenance />} />
+            <Route path="/fuel" element={<Fuel />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 };
 
 export default App;
