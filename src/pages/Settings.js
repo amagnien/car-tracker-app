15a5e9625cc9de01d9f6d2290a5ad0113301badf
amagnien <amagnien@gmail.com>
2025-01-29 13:26:09 +0400

    Create Settings.js

---
+++ src/pages/Settings.js
@@ -0,0 +1,15 @@
+// src/pages/Settings.js
+import React from 'react';
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <h2>Settings</h2>
+       <p>
+          (Will contain user settings and preferences)
+       </p>
+    </div>
+  );
+};
+
+export default SettingsPage;
