aed08026b6c980d0d753ff1ffa8c85407956e621
amagnien <amagnien@gmail.com>
2025-01-31 16:10:09 +0400

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,7 +7,6 @@ import Register from '../pages/Register';
 import CarManagement from '../pages/CarManagement';
 import Analytics from '../pages/Analytics';
 import NotFound from '../pages/NotFound';
-import { ToastProvider } from '../contexts/ToastContext';
 
 const PrivateRoute = ({ children }) => {
     const { user } = useAuth();
@@ -16,28 +15,26 @@ const PrivateRoute = ({ children }) => {
 
 const AppRoutes = () => {
     return (
-        <ToastProvider>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/" element={
-                    <PrivateRoute>
-                        <Dashboard />
-                    </PrivateRoute>
-                } />
-                <Route path="/cars" element={
-                    <PrivateRoute>
-                        <CarManagement />
-                    </PrivateRoute>
-                } />
-                <Route path="/analytics" element={
-                    <PrivateRoute>
-                        <Analytics />
-                    </PrivateRoute>
-                } />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </ToastProvider>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={
+                <PrivateRoute>
+                    <Dashboard />
+                </PrivateRoute>
+            } />
+            <Route path="/cars" element={
+                <PrivateRoute>
+                    <CarManagement />
+                </PrivateRoute>
+            } />
+            <Route path="/analytics" element={
+                <PrivateRoute>
+                    <Analytics />
+                </PrivateRoute>
+            } />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     );
 };
 
