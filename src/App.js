533ee096bc62d0ce3e4c26de45df4897e0f3984d
amagnien <amagnien@gmail.com>
2025-01-31 16:11:38 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,30 +7,27 @@ import Maintenance from './pages/Maintenance';
 import Expenses from './pages/Expenses';
 import Settings from './pages/Settings';
 import { AuthProvider } from './contexts/AuthContext';
-import { ToastProvider } from './contexts/ToastContext';
 import Toast from './components/Toast';
 import './App.css';
 
 function App() {
     return (
         <AuthProvider>
-            <ToastProvider>
-                <Router>
-                    <div className="min-h-screen bg-gray-100">
-                        <Navigation />
-                        <div className="container mx-auto px-4 py-8">
-                            <Routes>
-                                <Route path="/" element={<Dashboard />} />
-                                <Route path="/fuel" element={<FuelRecords />} />
-                                <Route path="/maintenance" element={<Maintenance />} />
-                                <Route path="/expenses" element={<Expenses />} />
-                                <Route path="/settings" element={<Settings />} />
-                            </Routes>
-                        </div>
-                        <Toast />
+            <Router>
+                <div className="min-h-screen bg-gray-100">
+                    <Navigation />
+                    <div className="container mx-auto px-4 py-8">
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/fuel" element={<FuelRecords />} />
+                            <Route path="/maintenance" element={<Maintenance />} />
+                            <Route path="/expenses" element={<Expenses />} />
+                            <Route path="/settings" element={<Settings />} />
+                        </Routes>
                     </div>
-                </Router>
-            </ToastProvider>
+                    <Toast />
+                </div>
+            </Router>
         </AuthProvider>
     );
 }
